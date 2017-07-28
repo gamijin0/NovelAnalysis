5de020ddbebc10e61eef8fd07268da4d19c818a1
chaos <497386856@qq.com>
2017-07-28 13:25:51 +0800

    统计每个人物在每段中出现的次数

--- character-pick-up.py
+++ character-pick-up.py
@@ -1,13 +1,16 @@
 import jieba
 import codecs
 import sys
+import time
+from progressbar import  ProgressBar
 
 
 class CharacterGraph:
     # properties
     name_list = {}  # 存储人物名字
     division_num = 0  # 按此数值将文本分段
-    section_list = []  # 存储文章段落
+    name_in_section = []  # 存储段落中人物的出现情况
+
 
     def __init__(self, nameFile, novleFile, division_num=3000, scope=sys.maxsize):
 
@@ -21,23 +24,34 @@ class CharacterGraph:
 
         # 将文本分段存储
         count = 0
-        section = []
+        nameInSection = {}
         with codecs.open(novleFile, encoding='utf-8', mode='r') as f:
-            for line in f.readlines()[:scope]:
+            lines = f.readlines()
+            bar = ProgressBar()
+            for i,line in enumerate(bar(lines[:scope])):
                 # 统计人物出现次数
                 for name in self.name_list:
                     if (name in line):
                         self.name_list[name] += 1
+                        #统计每段中人物的出现次数
+                        if (name not in nameInSection):
+                            nameInSection[name] = 1
+                        else:
+                            nameInSection[name]+=1
 
-                section.append(line.strip())
                 count += len(line.strip())
                 if (count > self.division_num):
                     count = 0
-                    self.section_list.append(section)
-                    section = []
+                    self.name_in_section.append(nameInSection)
+                    nameInSection = {}
+        time.sleep(0.01)
 
+    def getRelationship(self):
+        for section in self.name_in_section:
+            pass
 
 
 if (__name__ == "__main__"):
-    cg = CharacterGraph(nameFile='example/name_list.txt', novleFile="example/pfdsj.txt", scope=10000)
-    print([(x,cg.name_list[x]) for x in cg.name_list if cg.name_list[x]!=0])
+    cg = CharacterGraph(nameFile='example/name_list.txt', novleFile="example/pfdsj.txt")
+    print(cg.name_in_section)
+    # print([(x,cg.name_list[x]) for x in cg.name_list if cg.name_list[x]!=0])
