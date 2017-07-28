55be078b48adbf068d994a81819c3e428238e5fa
chaos <497386856@qq.com>
2017-07-28 18:52:26 +0800

    v1.0

--- character-pick-up.py
+++ character-pick-up.py
@@ -2,7 +2,7 @@ import jieba
 import codecs
 import sys
 import time
-from progressbar import  ProgressBar
+from progressbar import ProgressBar
 
 
 class CharacterGraph:
@@ -10,7 +10,7 @@ class CharacterGraph:
     name_list = {}  # 存储人物名字
     division_num = 0  # 按此数值将文本分段
     name_in_section = []  # 存储段落中人物的出现情况
-
+    relations = {}
 
     def __init__(self, nameFile, novleFile, division_num=3000, scope=sys.maxsize):
 
@@ -26,18 +26,19 @@ class CharacterGraph:
         count = 0
         nameInSection = {}
         with codecs.open(novleFile, encoding='utf-8', mode='r') as f:
+            print("Start reading the file:")
             lines = f.readlines()
             bar = ProgressBar()
-            for i,line in enumerate(bar(lines[:scope])):
+            for i, line in enumerate(bar(lines[:scope])):
                 # 统计人物出现次数
                 for name in self.name_list:
                     if (name in line):
                         self.name_list[name] += 1
-                        #统计每段中人物的出现次数
+                        # 统计每段中人物的出现次数
                         if (name not in nameInSection):
                             nameInSection[name] = 1
                         else:
-                            nameInSection[name]+=1
+                            nameInSection[name] += 1
 
                 count += len(line.strip())
                 if (count > self.division_num):
@@ -48,10 +49,52 @@ class CharacterGraph:
 
     def getRelationship(self):
         for section in self.name_in_section:
-            pass
+            if (len(section) <= 1):
+                continue
+            items = [x for x in section]
+            sorted(items)
+            s = 0
 
+            for i1, v1 in enumerate(items[:-2]):
+                for i2, v2 in enumerate(items[i1 + 1:]):
+                    if ((v1, v2) not in self.relations):
+                        self.relations.setdefault((v1, v2), section[v1]+section[v2])
+                    else:
+                        self.relations[(v1, v2)] += section[v1]+section[v2]
+
+
+            # for i1, v1 in enumerate(items[:-2]):
+            #     for i2, v2 in enumerate(items[i1 + 1:]):
+            #         s += section[v1] * section[v2]
+            # for i1, v1 in enumerate(items[:-2]):
+            #     for i2, v2 in enumerate(items[i1 + 1:]):
+            #         if ((v1, v2) not in self.relations):
+            #             self.relations.setdefault((v1, v2), section[v1] * section[v2] * 100.0 / s)
+            #         else:
+            #             self.relations[(v1, v2)] += section[v1] * section[v2] * 100.0 / s
+
+
+    def saveGephiData(self,threshold=0):
+        dirName = "Gephi"
+        import os
+        if(not os.path.exists(dirName)):
+            os.mkdir(dirName)
+        with codecs.open("%s/Nodes.txt" % dirName,encoding='utf-8',mode='w') as f:
+            f.write("Id Label Weight\n")
+            f.writelines(
+                ["%s %s %d\n" % (name,name,self.name_list[name]) for name in self.name_list if self.name_list[name]!=0]
+            )
+        with codecs.open("%s/Edges.txt" % dirName,encoding='utf-8',mode='w') as f:
+            f.write("Source Target Weight Type\n")
+            f.writelines(
+                ["%s %s %d undirected\n" % (tup[0],tup[1],self.relations[tup]) for tup in self.relations if self.relations[tup]>threshold]
+            )
+
+        print("Save finished.")
 
 if (__name__ == "__main__"):
     cg = CharacterGraph(nameFile='example/name_list.txt', novleFile="example/pfdsj.txt")
-    print(cg.name_in_section)
+    cg.getRelationship()
+    # print('\n'.join([str(x) + "=%d" % cg.relations[x] for x in cg.relations if (cg.relations[x] != 0)]))
     # print([(x,cg.name_list[x]) for x in cg.name_list if cg.name_list[x]!=0])
+    cg.saveGephiData(threshold=0)
